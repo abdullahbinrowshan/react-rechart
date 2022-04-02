a698b13be837682cdd3188e446ebb028a92f0638
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2022-04-02 12:54:00 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyLineChart from './components/MyLineChart/MyLineChart';
+import SpecialChart from './components/SpecialChart/SpecialChart';
+
+function App() {
+  return (
+    <div className="App">
+      <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
+    </div>
+  );
+}
+
+export default App;
